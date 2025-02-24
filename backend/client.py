115ce50a9cf579b16b6a7eee3954ab42ded02903
Duong147863 <duong147863@gmail.com>
2025-02-24 09:46:20 +0700

    Update File

--- backend/client.py
+++ backend/client.py
@@ -1,10 +1,10 @@
-# import requests
+import requests
 
-# url = "http://10.2.0.116:5000/extract_text"
-# image_path = "cccd.jpg"
+url = "http://10.2.0.116:5000/extract_text"
+image_path = "cccd.jpg"
 
-# with open(image_path, "rb") as img_file:
-#     files = {"image": img_file}
-#     response = requests.post(url, files=files)
+with open(image_path, "rb") as img_file:
+    files = {"image": img_file}
+    response = requests.post(url, files=files)
 
-# print(response.json())
+print(response.json())
